0e3ef7f36b2462f7c46cd434c42f2d9821706ad1
蒲悦蓉 <puyuerong@puyuerongdeMacBook-Pro.local>
2019-09-21 21:00:24 +0800

    第二次提交

--- MVCTest/MVCTest/Land/M/Modle.h
+++ MVCTest/MVCTest/Land/M/Modle.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface Modle : UIViewController
+@interface Modle : NSObject
 @property NSString *nameStr;
 @property NSString *psdStr;
 - (void)checkName:(NSString*)name password:(NSString*)psd;
